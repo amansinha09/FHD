2710af6cf0f221cf95f68028b2a96821c334e2fb
Aman <amansinha091@gmail.com>
2024-09-19 11:49:52 +0300

    first submission made ST2- got 0

--- scripts/starter.py
+++ scripts/starter.py
@@ -12,11 +12,14 @@ from sklearn.model_selection import train_test_split
 from utils import compute_score
 
 # Load the data
-data = pd.read_csv('../data/incidents_train.csv', index_col=0)
+#data = pd.read_csv('../data/incidents_train.csv', index_col=0)
+trainset = pd.read_csv('../data/incidents_train.csv', index_col=0)
+devset = pd.read_csv('../data/incidents_dev.csv', index_col=0)
+testset = pd.read_csv('../data/incidents_test.csv', index_col=0)
 
 # Split data
-trainset, testset = train_test_split(data, test_size=0.2, random_state=2024)
-print("#train_sample: ", len(trainset), " |#test_samples: ", len(testset))
+trainset, devset_ = train_test_split(trainset, test_size=0.2, random_state=2024)
+print("#train_sample: ", len(trainset), " |#dev_samples: ", len(devset)," |#test_samples: ", len(testset))
 
 #create sklearn pipeline
 text_clf_lr = Pipeline([
@@ -26,17 +29,24 @@ text_clf_lr = Pipeline([
 
 #evaluation
 predictions = {}
-for label in tqdm(['hazard-category', 'product-category', 'hazard', 'product']):
+dev_predictions = {}
+#for label in tqdm(['hazard-category', 'product-category', 'hazard', 'product']):
+for label in tqdm(['hazard', 'product']):
   print(label.upper())
   text_clf_lr.fit(trainset.title, trainset[label])
-  predictions[label] = text_clf_lr.predict(testset.title)
-  print(f"F1-micro: {f1_score(testset[label], predictions[label], average='micro'):.2f}")
-  print(f"F1-macro: {f1_score(testset[label], predictions[label], average='macro'):.2f}")
+  predictions[label] = text_clf_lr.predict(devset_.title)
+  dev_predictions[label] = text_clf_lr.predict(devset.title)
+  print(f"DEV F1-micro: {f1_score(devset_[label], predictions[label], average='micro'):.2f}")
+  print(f"DEV F1-macro: {f1_score(devset_[label], predictions[label], average='macro'):.2f}")
 
 
-for label in ('hazard-category', 'product-category', 'hazard', 'product'):
-  testset['lr-'+label] = predictions[label]
+#save predictions
+solution = pd.DataFrame({'hazard':dev_predictions['hazard'], 'product':dev_predictions['product']})
+solution.to_csv('../submission_folder/demo_dev.csv', index=False)
+print("submission created!")
+#for label in ('hazard-category', 'product-category', 'hazard', 'product'):
+#  devset['lr-'+label] = predictions[label]
 
-print('Score Sub-Task 1:', compute_score(testset['hazard-category'], testset['product-category'], testset['lr-hazard-category'], testset['lr-product-category']))
-print('Score Sub-Task 2:', compute_score(testset['hazard'], testset['product'], testset['lr-hazard'], testset['lr-product']))
+#print('Score Sub-Task 1:', compute_score(testset['hazard-category'], testset['product-category'], testset['lr-hazard-category'], testset['lr-product-category']))
+#print('Score Sub-Task 2:', compute_score(testset['hazard'], testset['product'], testset['lr-hazard'], testset['lr-product']))
 
